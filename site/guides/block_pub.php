efbcbf99628837e7c388ecdf4e42ea4dabb371c5
Lilian Hiault <lilian.hiault@laposte.net>
2021-10-16 16:56:11 +0200

    Corrections path jquery

--- site/guides/block_pub.php
+++ site/guides/block_pub.php
@@ -7,7 +7,7 @@
     <title> Bloqueur de Pub </title>
     <link rel="stylesheet" type="text/css" href="../style.css" />
 
-    <script src="./jquery/jquery.js"></script>
+    <script src="../jquery/jquery.js"></script>
 </head>
 
 <body>
